b328bc8654429f7c7618ec9be39d5fd67c44fc13
Douglas Resende <douglasmresende@gmail.com>
2024-03-12 23:19:17 -0300

    adicionado scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-function Home(){
-    return<>
+function Home() {
+  return;
+  <>
     <h1>Eu te amo!</h1>
     <h2>Eu não conseguiria sem o teu apoio!</h2>
-    </> 
+  </>;
 }
 
 export default Home;
