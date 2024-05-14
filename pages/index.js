b5a100b525042747a7fbd93b29328542b663e182
Douglas Resende <douglasmresende@gmail.com>
2024-05-13 22:54:54 -0300

    install  and configure test script

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 function Home() {
   return (
     <>
-      <h1>Eu te amo!</h1>
-      <h2>Eu não conseguiria sem o teu apoio!</h2>
+      <h1>Hpme sreem</h1>
+      <h2>Em desenvolvimento . . .</h2>
     </>
   );
 }
 
 export default Home;
+
